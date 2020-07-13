cfab3cf65a4ce8a2fc908917da7e2654c6442c00
Daniel <dtues003@fiu.edu>
2020-07-13 14:59:49 -0400

    added pagination and crud for bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,36 +1,93 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
+const Bootcamp = require("../models/Bootcamp");
 
-//@desc     Get all bootcamps
-//@route    GET /api/v1/bootcamps
-//@access   Public
-exports.getBootcamps = (req, res, next) => {
-   res.status(200).json({ success: true, msg: "show all bootcamps" });
-}
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+	const bootcamps = await Bootcamp.find();
+	res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
-//@desc     Get single bootcamp
-//@route    GET /api/v1/bootcamps/:id
-//@access   Public
-exports.getBootcamp = (req, res, next) => {
-   res.status(200).json({ success: true, msg: "get bootcamp " + req.params.id });
-}
+// @desc      Get single bootcamp
+// @route     GET /api/v1/bootcamps/:id
+// @access    Public
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.findById(req.params.id);
 
-//@desc     Create new bootcamp
-//@route    POST /api/v1/bootcamps
-//@access   Private
-exports.createBootcamp = (req, res, next) => {
-   res.status(200).json({ success: true, msg: "create new bootcamp" });
-}
+	if (!bootcamp) {
+		return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+	}
 
-//@desc     Update bootcamp
-//@route    PUT /api/v1/bootcamps/:id
-//@access   Private
-exports.updateBootcamp = (req, res, next) => {
-	res.status(200).json({ success: true, msg: "update bootcamp " + req.params.id });
-}
+	res.status(200).json({ success: true, data: bootcamp });
+});
 
-//@desc     Delete bootcamp
-//@route    PUT /api/v1/bootcamps/:id
-//@access   Private
-exports.deleteBootcamp = (req, res, next) => {
-	res.status(200).json({ success: true, msg: "delete bootcamp " + req.params.id });
-}
+// @desc      Create new bootcamp
+// @route     POST /api/v1/bootcamps
+// @access    Private
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.create(req.body);
 
+	res.status(201).json({
+		success: true,
+		data: bootcamp,
+	});
+});
+
+// @desc      Update bootcamp
+// @route     PUT /api/v1/bootcamps/:id
+// @access    Private
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+	let bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	if (!bootcamp) {
+		return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+	}
+
+	res.status(200).json({ success: true, data: bootcamp });
+});
+
+// @desc      Delete bootcamp
+// @route     DELETE /api/v1/bootcamps/:id
+// @access    Private
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+	const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+	if (!bootcamp) {
+		return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+	}
+
+	res.status(200).json({ success: true, data: {} });
+});
+
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+	const { zipcode, distance } = req.params;
+
+	// Get lat/lng from geocoder
+	const loc = await geocoder.geocode(zipcode);
+	const lat = loc[0].latitude;
+	const lng = loc[0].longitude;
+
+	// Calc radius using radians
+	// Divide dist by radius of Earth
+	// Earth Radius = 3,963 mi / 6,378 km
+	const radius = distance / 3963;
+
+	const bootcamps = await Bootcamp.find({
+		location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+	});
+
+	res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		data: bootcamps,
+	});
+});
