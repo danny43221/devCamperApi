db229f9ce94ecc63286474dcb1ec339cbf83535a
Daniel <dtues003@fiu.edu>
2020-07-15 17:27:23 -0400

    add protect middleware

--- models/User.js
+++ models/User.js
@@ -41,8 +41,13 @@ UserSchema.pre("save", async function (next) {
 //Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
 	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE
-   });
+		expiresIn: process.env.JWT_EXPIRE,
+	});
+};
+
+//Match user entered password to hashed password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+   return await bcrypt.compare(enteredPassword, this.password)
 };
 
 module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser")
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -21,6 +22,9 @@ const app = express();
 
 //Body Parser
 app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser())
 
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
