73204d3c390529eef620b7222b9d9c10db58fe11
Daniel <dtues003@fiu.edu>
2020-07-17 13:19:55 -0400

    added roles

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,18 @@ exports.login = asyncHandler(async (req, res, next) => {
    sendTokenResponse(user, 200, res)
 });
 
+// @desc		  Get current logged in user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user.id)
+
+	res.status(200).json({
+		success: true,
+		data: user
+	})
+})
+
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
 	// Create token
@@ -61,3 +73,5 @@ const sendTokenResponse = (user, statusCode, res) => {
 
 	res.status(statusCode).cookie("token", token, options).json({ success: true, token });
 };
+
+
