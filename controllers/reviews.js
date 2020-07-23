4646a81ccbd5663e2307a2dc525a8756d7638c41
Daniel <dtues003@fiu.edu>
2020-07-23 17:29:16 -0400

    update and delete reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -50,12 +50,61 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
 	if (!bootcamp) {
 		return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, 404));
-   }
-   
-   const review = await Review.create(req.body)
+	}
+
+	const review = await Review.create(req.body);
 
 	res.status(201).json({
 		success: true,
 		data: review,
+	});
+});
+
+// @desc      update review
+// @route     PUT /api/v1/reviews/:id
+// @access    Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+	let review = await Review.findById(req.params.id);
+
+	if (!review) {
+		return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404));
+   }
+   
+   //Make sure review belongs to user or user is admin
+   if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return next(new ErrorResponse(`Not authorized to update review`, 401));
+   }
+
+   review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+   })
+
+	res.status(201).json({
+		success: true,
+		data: review,
+	});
+});
+
+// @desc      delete review
+// @route     DELETE /api/v1/reviews/:id
+// @access    Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+	const review = await Review.findById(req.params.id);
+
+	if (!review) {
+		return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404));
+   }
+   
+   //Make sure review belongs to user or user is admin
+   if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return next(new ErrorResponse(`Not authorized to update review`, 401));
+   }
+
+   await review.remove();
+
+	res.status(201).json({
+		success: true,
+		data: {},
 	});
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {
-   getReviews,
-   getReview,
-   addReview
+	getReviews,
+	getReview,
+	addReview,
+	updateReview,
+	deleteReview,
 } = require("../controllers/reviews");
 
 const Review = require("../models/Review");
@@ -14,9 +16,13 @@ const { protect, authorize } = require("../middleware/auth");
 
 router
 	.route("/")
-   .get(advancedResults(Review, { path: "bootcamp", select: "name description" }), getReviews)
-   .post(protect, authorize('user', 'admin'), addReview)
-   
-router.route("/:id").get(getReview)
+	.get(advancedResults(Review, { path: "bootcamp", select: "name description" }), getReviews)
+	.post(protect, authorize("user", "admin"), addReview);
+
+router
+	.route("/:id")
+	.get(getReview)
+	.put(protect, authorize("user", "admin"), updateReview)
+	.delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
